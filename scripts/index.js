67b6409bc466713d36c56e0f4232b4c6bc5e21a6
aKrivonogova <krivonogova.aline@yandex.ru>
2023-06-25 20:24:08 +0300

    исправлено полоение ссылок на меню

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 const burgerMenuContent = document.querySelector('.header__content');
 const burgerButton = document.querySelector('.header__burger');
 const body = document.querySelector('body');
-
+// клик по кнопке меню 
 burgerButton.addEventListener('click', () => {
     burgerMenuContent.classList.toggle('header__menu-active');
     burgerButton.classList.toggle('is-active');
 })
 
+// клик по пустому пространству 
 body.addEventListener('click', (event) => {
     const currentMenuBlock = event.composedPath().includes(burgerMenuContent);
     const burgerMenuBlock = event.composedPath().includes(burgerButton);
