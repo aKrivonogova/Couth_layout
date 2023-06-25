de095d08e78b776765cf2312ec4ff5b2e6d49c57
aKrivonogova <krivonogova.aline@yandex.ru>
2023-06-25 20:17:29 +0300

    добавлено меню бургер

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+const burgerMenuContent = document.querySelector('.header__content');
+const burgerButton = document.querySelector('.header__burger');
+const body = document.querySelector('body');
+
+burgerButton.addEventListener('click', () => {
+    burgerMenuContent.classList.toggle('header__menu-active');
+    burgerButton.classList.toggle('is-active');
+})
+
+body.addEventListener('click', (event) => {
+    const currentMenuBlock = event.composedPath().includes(burgerMenuContent);
+    const burgerMenuBlock = event.composedPath().includes(burgerButton);
+
+    if (!currentMenuBlock && !burgerMenuBlock) {
+        burgerButton.classList.remove('is-active');
+        burgerMenuContent.classList.remove('header__menu-active');
+    }
+})
